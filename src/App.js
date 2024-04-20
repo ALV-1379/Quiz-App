001b265ba1d9f35f03ac1b5059d21a93ffd14100
ALV-1379 <alavimohammad93@gmail.com>
2024-04-20 05:48:30 -0700

    quiz app with react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Quiz from './components/Quiz/Qiuz'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Quiz />
+            </div>
+        )
+    }
+}
